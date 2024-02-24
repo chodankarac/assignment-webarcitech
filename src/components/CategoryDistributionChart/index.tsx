248381f72a27bc07804da38f73d89fb0dcd2ab8f
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2024-02-24 22:58:27 +0530

    css added and code restructured;implements issue #3

--- src/components/CategoryDistributionChart/index.tsx
+++ src/components/CategoryDistributionChart/index.tsx
@@ -8,15 +8,15 @@ const CategoryDistributionChart = () => {
     <div className="ChartContainer">
       <h1 className="ChartTitle">Category Distribution</h1>
       <BarChart
-        width={450}
+        width={650}
         height={300}
         data={data}
         margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
         barSize={20}
         barGap={1}
       >
-        <XAxis dataKey="category" tick={{ fontSize: 10 }} />
-        <YAxis tick={{ fontSize: 10 }} />
+        <XAxis dataKey="category" tick={{ fontSize: 15 }} />
+        <YAxis tick={{ fontSize: 15 }} />
         <Tooltip />
         <Legend />
         <Bar dataKey="queries" fill="#8884d8" />
